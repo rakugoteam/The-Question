8a500978aa386a2b2fbb3599dc876c2b0cd4379d
Jeremi Biernacki <jeremi360@gmail.com>
2025-04-17 16:49:51 +0200

    make ask use say panel

--- scenes/DialogueUI/AskPopup.gd
+++ scenes/DialogueUI/AskPopup.gd
@@ -1,23 +1,20 @@
 @tool
 class_name AskPopup
-extends DialoguePanel
+extends ProcentControl
 
-@export var line_edit : LineEdit
+@export var say_panel: SayPanel
+@export var line_edit: LineEdit
 
 func _ready():
-	if Engine.is_editor_hint():
-		return
-	
-	super._ready()
+	if Engine.is_editor_hint(): return
 	Rakugo.sg_ask.connect(_on_ask)
 
-func _on_ask(character:Dictionary, question:String, default_answer:String):
-	set_labels(character, question)
+func _on_ask(character: Dictionary, question: String, default_answer: String):
 	line_edit.placeholder_text = default_answer
+	say_panel.set_labels(character, question)
 
-func _process(_delta):
-	if Engine.is_editor_hint():
-		return
+func _process(_delta: float):
+	if Engine.is_editor_hint(): return
 
 	if Input.is_action_just_pressed("ui_accept"):
 		_on_ok_btn_pressed()
@@ -27,12 +24,10 @@ func _on_default_answer_btn_pressed():
 	hide()
 
 func _on_ok_btn_pressed():
-	if line_edit.text:
-		Rakugo.ask_return(line_edit.text)
-	else:
-		Rakugo.ask_return(line_edit.placeholder_text)
+	if line_edit.text: Rakugo.ask_return(line_edit.text)
+	else: Rakugo.ask_return(line_edit.placeholder_text)
 	hide()
 
-func _on_text_submitted(new_text):
+func _on_text_submitted(new_text: String):
 	Rakugo.ask_return(new_text)
 	hide()
