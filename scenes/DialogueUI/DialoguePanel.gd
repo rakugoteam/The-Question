9d02181a746b2cd1aecec70cf5d439d10c95f430
Jeremi Biernacki <jeremi360@gmail.com>
2025-06-14 13:49:23 +0200

    fixed coloring of character labels

--- scenes/DialogueUI/DialoguePanel.gd
+++ scenes/DialogueUI/DialoguePanel.gd
@@ -9,39 +9,25 @@ var markup: TextParser
 
 func _ready():
 	markup = load(VisualNovelKit.default_markup_setting)
-	if character_name_label:
-		character_name_label.parser = markup
-	
-	if dialogue_label:
-		dialogue_label.parser = markup
-
-	visibility_changed.connect(_on_visibility_changed)
-	
+	if character_name_label: character_name_label.parser = markup
+	if dialogue_label: dialogue_label.parser = markup
 	visible = Engine.is_editor_hint()
-	set_process(false)
 
 func set_labels(character: Dictionary, text: String):
-	show()
-	
+	await get_tree().create_timer(0.1).timeout
+	if !visible: show()
 	var character_name = character.get("name", "")
-
-	if not character_name:
-		character_name_label.advanced_text = ""
+	var character_color = character.get("color", null)
 	
-	else:
-		var name_label = "[h1]%s[/h1]" % character_name
-
-		if markup is MarkdownParser:
+	var name_label := "[h1]%s[/h1]" % character_name
+	if character_color: name_label = "[color=%s]%s[/color]" % [character_color, name_label]
+	
+	if markup is MarkdownParser:
+		if !character_name: name_label = ""
+		else:
 			name_label = "# %s\n" % character_name
+			if character_color:
+				name_label = "# [color=%s]%s[/color]\n" % [character_color, character_name]
 
-		var character_color = character.get("color", null)
-		
-		if character_color:
-			name_label = "[color=%s]%s[/color]" % [character_color, name_label]
-		
-		character_name_label.advanced_text = name_label
-	
+	character_name_label.advanced_text = name_label
 	dialogue_label.advanced_text = text
-
-func _on_visibility_changed():
-	set_process(visible)
