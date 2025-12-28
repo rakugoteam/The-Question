add7fdc6df72c4c08df1fd6ae9d1d8d25322abb8
Jeremi Biernacki <jeremi360@gmail.com>
2025-12-28 13:58:13 +0100

    first implementation of saving dialogue state

--- addons/Rakugo/lib/systems/Executer.gd
+++ addons/Rakugo/lib/systems/Executer.gd
@@ -267,6 +267,7 @@ func do_execute_script(parameters: Dictionary):
 				
 				foo.call_deferred()
 		
+		Rakugo.set_variable("_dialogue", Rakugo.last_thread_data)
 		index += 1
 	
 	call_deferred("do_execute_script_end", parameters)
